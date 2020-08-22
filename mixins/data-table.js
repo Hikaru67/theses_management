33514d02accb6d6f5e5ffe52db3656b333d9fd54
Pham Anh Duc <vndhero@gmail.com>
2020-08-22 21:12:14 +0700

    🛠 Full CRUD

--- mixins/data-table.js
+++ mixins/data-table.js
@@ -2,23 +2,20 @@
  * Copyright 2020 Digi Dinos JSC. All rights reserved.
  * Email: tech@digidinos.com.
  */
+
+import { camelCase } from 'lodash'
+
 export default {
   async fetch() {
-    const params = {
-      limit: +this.$route.query.limit || 10,
-      page: +this.$route.query.page || 1,
-      sortOrder: this.$route.query.sortOrder,
-      sortField: this.$route.query.sortField
-    }
-
+    const params = this.$route.query
     this.loading = true
     const action = `${this.resource}/getList`
     const { data, meta } = await this.$store.dispatch(action, { params })
     this.pagination = {
       ...this.pagination,
       total: meta ? meta.total : data.length,
-      current: params.page,
-      pageSize: params.limit,
+      current: +params.page,
+      pageSize: +params.limit,
       showTotal: total => `Total ${total} items`
     }
     this.data = data
@@ -47,7 +44,48 @@ export default {
         sortOrder: sorter.order,
         sortField: sorter.field
       }
-      this.$router.replace({ query })
+      this.replaceQuery(query)
+    },
+
+    replaceQuery(data) {
+      const query = {}
+      const newQuery = {
+        ...this.$route.query,
+        ...data
+      }
+      Object.entries(newQuery).forEach(([key, value]) => {
+        if (value !== '' && value !== null && value !== undefined) {
+          query[key] = value
+        }
+      })
+      if (JSON.stringify(query) === JSON.stringify(this.$route.query)) {
+        this.$fetch()
+      } else {
+        this.$router.replace({ query })
+      }
+    },
+
+    goToDetail(id) {
+      this.$router.push(`/${this.resource}/${id}`)
+    },
+
+    confirmToDelete(id) {
+      this.$confirm({
+        title: 'Are you sure delete this task?',
+        content: 'Some descriptions',
+        okText: 'Yes',
+        okType: 'danger',
+        cancelText: 'No',
+        onOk: () => this.deleteRecord(id)
+      })
+    },
+
+    async deleteRecord(id) {
+      this.loading = true
+      const action = camelCase(`delete-${this.resource}`)
+      await this.$api[action]({ id })
+      this.loading = false
+      this.$fetch()
     }
   }
 }
--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,38 +1,185 @@
 <template>
-  <a-table
-    :columns="columns"
-    :row-key="record => record.id"
-    :data-source="data"
-    :pagination="pagination"
-    :loading="loading"
-    @change="handleTableChange"
-  >
-    <span
-      slot="action"
-      slot-scope="record"
+  <div>
+    <a-form-model
+      ref="form"
+      v-loading="loading"
+      :model="filters"
+      :label-col="{ sm: 6 }"
+      :wrapper-col="{ sm: 18 }"
+      class="search-form"
+      @submit.prevent="search"
     >
-      <a-button
-        type="primary"
-        :disabled="loading"
-        @click="goToDetail(record.id)"
+      <a-row
+        type="flex"
+        :gutter="30"
       >
-        <font-awesome-icon
-          icon="eye"
-          class="width-1x"
-        />
-      </a-button>
-    </span>
-  </a-table>
+        <a-col
+          :span="24"
+          :md="12"
+        >
+          <a-form-model-item
+            :label="$t('user.name')"
+            prop="name"
+          >
+            <a-input
+              v-model="filters.name"
+              :placeholder="$t('user.name')"
+              :disabled="loading"
+            >
+              <font-awesome-icon
+                slot="addonBefore"
+                icon="user"
+                class="width-1x"
+              />
+            </a-input>
+          </a-form-model-item>
+        </a-col>
+
+        <a-col
+          :span="24"
+          :md="12"
+        >
+          <a-form-model-item
+            :label="$t('user.email')"
+            prop="email"
+          >
+            <a-input
+              v-model="filters.email"
+              :placeholder="$t('user.email')"
+              :disabled="loading"
+            >
+              <font-awesome-icon
+                slot="addonBefore"
+                icon="envelope"
+                class="width-1x"
+              />
+            </a-input>
+          </a-form-model-item>
+        </a-col>
+
+        <a-col
+          :md="24"
+          class="box-form-footer text-center p-3"
+        >
+          <a-button
+            html-type="submit"
+            type="primary"
+            :disabled="loading"
+            class="w-min-100"
+          >
+            <font-awesome-icon
+              icon="search"
+              class="width-1x mr-1"
+            />
+            {{ $t('common.search') }}
+          </a-button>
+
+          &nbsp;
+          <a-button
+            html-type="button"
+            type="default"
+            :disabled="loading"
+            class="w-min-100"
+            @click="reset"
+          >
+            <font-awesome-icon
+              icon="eraser"
+              class="width-1x mr-1"
+            />
+            {{ $t('common.clear') }}
+          </a-button>
+        </a-col>
+      </a-row>
+    </a-form-model>
+    <a-button
+      type="primary"
+      :disabled="loading"
+      @click="showDetail(0)"
+    >
+      Create User
+    </a-button>
+    <a-table
+      :columns="columns"
+      :row-key="record => record.id"
+      :data-source="data"
+      :pagination="pagination"
+      :loading="loading"
+      @change="handleTableChange"
+    >
+      <span
+        slot="action"
+        slot-scope="record"
+      >
+        <a-button
+          type="success"
+          :disabled="loading"
+          @click="goToDetail(record.id)"
+        >
+          <font-awesome-icon
+            icon="pencil-alt"
+            class="width-1x"
+          />
+        </a-button>
+        <a-button
+          type="primary"
+          :disabled="loading"
+          @click="showDetail(record.id)"
+        >
+          <font-awesome-icon
+            icon="eye"
+            class="width-1x"
+          />
+        </a-button>
+        <a-button
+          type="danger"
+          :disabled="loading"
+          @click="confirmToDelete(record.id)"
+        >
+          <font-awesome-icon
+            icon="times"
+            class="width-1x"
+          />
+        </a-button>
+      </span>
+    </a-table>
+    <a-modal
+      ref="detail"
+      :visible="visible"
+      :footer="null"
+      class="modal-wrap"
+    >
+      <template slot="title">
+        <font-awesome-icon :icon="`${currentId ? 'pencil-alt' : 'plus-circle'}`" />
+        {{ currentId ? $t('user.edit_user') : $t('user.create_new_user') }}
+      </template>
+
+      <user-form
+        :id="currentId"
+        @save="closeDialog(true)"
+        @cancel="closeDialog(false)"
+      />
+    </a-modal>
+  </div>
 </template>
 <script>
 
 import DataTable from '~/mixins/data-table'
+import UserForm from '~/components/organisms/UserForm'
 
 export default {
+  components: { UserForm },
   mixins: [DataTable],
-  data: () => ({
-    resource: 'user'
-  }),
+  data() {
+    return {
+      resource: 'user',
+      visible: false,
+      currentId: 0,
+      filters: {
+        name: this.$route.query.name || '',
+        email: this.$route.query.email || ''
+      }
+    }
+  },
   computed: {
     columns() {
       const columns = [
@@ -62,8 +209,30 @@ export default {
     }
   },
   methods: {
-    goToDetail(id) {
-      this.$router.push(`/user/${id}`)
+    showDetail(id) {
+      this.currentId = id
+      this.visible = true
+    },
+
+    closeDialog(fetch) {
+      this.visible = false
+      if (fetch) {
+        this.$fetch()
+      }
+    },
+
+    /**
+     * On clear form search
+     */
+    reset() {
+      this.filters = {
+        name: '',
+        email: ''
+      }
+    },
+
+    search() {
+      this.replaceQuery(this.filters)
     }
   }
 }
