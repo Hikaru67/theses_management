b6a570ae8f738d9ba9dcfeb572f0941882c6f340
duc.pa <duc.pa@digidinos.com>
2020-09-22 18:06:32 +0700

    🛩 Import partial

--- plugins/antd-ui.js
+++ plugins/antd-ui.js
@@ -4,6 +4,28 @@
  */
 
 import Vue from 'vue'
-import Antd from 'ant-design-vue/lib'
+// import Antd from 'ant-design-vue/lib'
+import { Breadcrumb, Button, Card, Checkbox, Col, Collapse, Dropdown, FormModel, Icon, Input, Layout, Menu, Modal, Row, Select, Spin, Table, Tree, notification } from 'ant-design-vue'
 
-Vue.use(Antd)
+// Vue.use(Antd)
+Vue.use(Breadcrumb)
+Vue.use(Button)
+Vue.use(Card)
+Vue.use(Checkbox)
+Vue.use(Col)
+Vue.use(Collapse)
+Vue.use(Dropdown)
+Vue.use(FormModel)
+Vue.use(Icon)
+Vue.use(Input)
+Vue.use(Layout)
+Vue.use(Menu)
+Vue.use(Modal)
+Vue.use(Row)
+Vue.use(Select)
+Vue.use(Spin)
+Vue.use(Table)
+Vue.use(Tree)
+
+Vue.prototype.$notification = notification
+Vue.prototype.$confirm = Modal.confirm
